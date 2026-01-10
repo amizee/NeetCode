da59ff4e1848edbe9e7f292dbe67e62e71ade330
Kevin Kong <kkon0188@uni.sydney.edu.au>
2026-01-10 20:51:03 +1100

    medium linked list

---
+++ Linked Lists/Reorder Linked List.py
@@ -0,0 +1,26 @@
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        # Find the halfway point 
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second = slow.next # Right half that needs to be reversed can be smaller than the left half
+        slow.next = None # "Splits" the list in half
+        prev = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+        
+        # Merge two halves
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next 
+            first.next = second 
+            second.next = tmp1
+            first, second = tmp1, tmp2
+
+# Intuition: Reverse the right half, then merge the two halves into one list
