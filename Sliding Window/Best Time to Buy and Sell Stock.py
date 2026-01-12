45d32d34aebf067d667d7f1fbd273856ab546d18
Kevin Kong <kkon0188@uni.sydney.edu.au>
2026-01-12 17:33:15 +1100

    sliding window

---
+++ Sliding Window/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,27 @@
+# Sliding window
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxProfit = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                maxProfit = max(maxProfit, prices[r] - prices[l])
+            else:
+                l = r
+            r += 1
+        return maxProfit
+
+# One pass, keeping track of the minimum price so far (highest potential profit)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0 
+        minPrice = prices[0]
+        for i in range(len(prices)):
+            if prices[i] > minPrice:
+                maxProfit = max(maxProfit, prices[i] - minPrice)
+            else:
+                minPrice = prices[i]
+        return maxProfit
+            
+
+        
